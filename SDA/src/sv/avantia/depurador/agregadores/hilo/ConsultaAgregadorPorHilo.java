9c7e9e3d5629ef6c6cbc537a75f6e1ad55e47e2c
emejia1SV <edwinmejia_1@hotmail.com>
2014-09-13 17:29:23 -0600

    nueva implementacion de cliente
    probar los servicos de claro

--- SDA/src/sv/avantia/depurador/agregadores/hilo/ConsultaAgregadorPorHilo.java
+++ SDA/src/sv/avantia/depurador/agregadores/hilo/ConsultaAgregadorPorHilo.java
@@ -12,6 +12,7 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -19,6 +20,7 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+import javax.xml.rpc.encoding.XMLType;
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.MimeHeaders;
 import javax.xml.soap.SOAPConnection;
@@ -40,6 +42,7 @@ import sv.avantia.depurador.agregadores.entidades.Agregadores;
 import sv.avantia.depurador.agregadores.entidades.Metodos;
 import sv.avantia.depurador.agregadores.entidades.Parametros;
 import sv.avantia.depurador.agregadores.ws.cliente.Cliente;
+import sv.avantia.depurador.agregadores.ws.cliente.WebServicesClient;
 
 import com.cladonia.xml.webservice.soap.SOAPClient;
 import com.cladonia.xml.webservice.wsdl.WSDLException;
@@ -114,109 +117,47 @@ public class ConsultaAgregadorPorHilo extends Thread {
 		this.agregador = agregador;
 	}
 	
-	/*private void procesarResultados(){
-		try {
-			if (getInsumos() != null && 0 < getInsumos().length && getInsumos()[0] != null) {
-				ResultSet resultSet0 = getInsumos()[0];
-				ResultSet resultSet1 = getInsumos()[1];
-				ResultSet resultSet2 = getInsumos()[2];
-				resultSet0.first();
-				while(resultSet0.next()){
-					if (getInsumos() != null && 1 < getInsumos().length && getInsumos()[1] != null) {
-						resultSet1.first();
-						while(resultSet1.next()){
-							if (getInsumos() != null && 2 < getInsumos().length && getInsumos()[2] != null) {
-								resultSet2.first();
-								while(resultSet2.next()){
-									if (getInsumos() != null && 3 < getInsumos().length && getInsumos()[3] != null) {
-										while(getInsumos()[3].next()){
-											if (getInsumos() != null && 4 < getInsumos().length && getInsumos()[4] != null) {
-												while(getInsumos()[4].next()){
-													if (getInsumos() != null && 5 < getInsumos().length && getInsumos()[5] != null) {
-														while(getInsumos()[5].next()){
-															
-														}
-													}
-												}
-											}
-										}
-									}else{
-										try {
-											if (getParametrizacion() != null) {				
-												if(getParametrizacion().getServicios()!=null){
-													for (Servicios servicio : getParametrizacion().getServicios()) {
-														if(servicio.getMetodos()!=null){
-															for (Metodos metodo : servicio.getMetodos()) {
-																if(metodo.getParametros()!=null){
-																	for (Parametros parametro : metodo.getParametros()) {
-																		
-																		
-																		if (parametro.getInsumo().equals(resultSet0.getString("INSUMO"))) {
-																			metodo.getOperacionSRV().setInputMessageText(
-																					metodo.getOperacionSRV().getInputMessageText().replace(("_*"+parametro.getNombre()+"_*"), "" + Class.forName(parametro.getTipo()).getConstructor(String.class).newInstance(resultSet0.getString(parametro.getColumna().toUpperCase())))
-																					);
-																		}else if (parametro.getInsumo().equals(resultSet1.getString("INSUMO"))) {
-																			metodo.getOperacionSRV().setInputMessageText(
-																					metodo.getOperacionSRV().getInputMessageText().replace(("_*"+parametro.getNombre()+"_*"), "" + Class.forName(parametro.getTipo()).getConstructor(String.class).newInstance(resultSet1.getString(parametro.getColumna().toUpperCase())))
-																					);
-																		}else if (parametro.getInsumo().equals(resultSet2.getString("INSUMO"))) {
-																			metodo.getOperacionSRV().setInputMessageText(
-																					metodo.getOperacionSRV().getInputMessageText().replace(("_*"+parametro.getNombre()+"_*"), "" + Class.forName(parametro.getTipo()).getConstructor(String.class).newInstance(resultSet2.getString(parametro.getColumna().toUpperCase())))
-																					);
-																		}	
-																	}
-																}
-																obtenerRespuesta(Cliente.invokeOperation(metodo.getOperacionSRV()));
-																System.out.println();
-																	}
-																	
-																}
-															}
-														}
-											}
-										} catch (ClassNotFoundException e) {
 
-										} catch (IllegalArgumentException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										} catch (SecurityException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										} catch (InstantiationException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										} catch (IllegalAccessException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										} catch (InvocationTargetException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										} catch (NoSuchMethodException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										}
-									}
-								}
-							}
-						}
-						
-					}else{
-						System.out.println("no hay datos que procesar");
-					}
-				}
-				resultSet0.close();
-				resultSet0 = null;
-				resultSet1.close();
-				resultSet1 = null;
-				resultSet2.close();
-				resultSet2 = null;
-			} else {
-				System.out.println("Hasta aqui llegue porque no hay data que procesar");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}*/
+	//*********************************************************************************************************************************/
+	//		cliente SIN SSL es un cliente normal sin seguridad RPC
+	//*********************************************************************************************************************************/
+
+	
+	/**
+	 * Llenar con los parametros requeridos para el llamdo de el web services
+	 * para la comunicacion con los servicios parametrizados
+	 * 
+	 * @author Emejia - Avantia Consultores
+	 * @param definitionArgument
+	 *            - es en objeto vienen los dos marametros listos para consumir
+	 *            el web services SMG3 donde viene el objeto y el dato para el
+	 *            trace level del SMG3
+	 * @param operacion
+	 *            - este es el nombre de la operacion que se va a efectuar
+	 *            dentro de servicio web ya sea para consultar una estrategia o
+	 *            para actualizar dichas estrategias
+	 * @throws Exception
+	 * */
+	public String consultarServicio(Map<String, Object> definitionArgument, String operacion) throws Exception{
+		WebServicesClient stub = new WebServicesClient();
+		//1st argument service URI, refer to wsdl document above
+		//2nd argument is service name, refer to wsdl document above
+		//QName qname = new QName("http://webservices.smg3.bbmass.com.sv/", "DACallWSService");
+		//Localmente >> stub.setAddress("http://sv01d000n6116:9081/SMG3_HTTP/DACallWSService");
+		//DESARROLLO CLUSTER >> stub.setAddress("http://sv4044aap.daviviendasv.com:9121/SMG3_HTTP/DACallWSService");
+		//BUS >>stub.setAddress("http://sv4012lap.daviviendasv.com/SMG3_HTTP/DACallWSService");
+		stub.setAddress("http://sv4012lap/SMG3_HTTP/DACallWSService");
+		stub.setNamespaceURI("http://webservices.smg3.bbmass.com.sv/");
+		stub.setReturnType(XMLType.XSD_STRING);//XMLType.XSD_STRING or Qname
+		stub.setServiceName("DACallWSService");
+		stub.setPortName("DACallWSPort");			
+		stub.setDefinitionArgument(definitionArgument);
+		stub.setOpertationNameInvoke(operacion); //"DACallPREBURO"
+		stub.setTimeOutSeconds(6000);
+		
+		return (String) stub.respuestaWebService();
+	}
+	
 	//*********************************************************************************************************************************/
 	//		cliente SIN SSL es un cliente normal sin seguridad
 	//*********************************************************************************************************************************/
